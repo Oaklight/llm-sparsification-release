395b2c255bcf1269e12aab900636d7d3cc814a98
Cole Frank <cole.v.frank@gmail.com>
2022-11-09 09:10:19 -0600

    new src/ file for pruning code

---
+++ src/pruning_utils.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+
+
+def prune_gpt2_layers(ratio):
+
+    for i in range(48):
+        list= ['h.{}.attn.c_attn'.format(i),
+                'h.{}.attn.c_proj'.format(i),
+                #'h.{}.ln_1'.format(i), leave layer norms unpruned
+                #'h.{}.ln_2'.format(i),
+                'h.{}.mlp.c_fc'.format(i),
+                'h.{}.mlp.c_proj'.format(i)]
+        for name, module in model.named_modules():
+            if name in list:
+                prune.l1_unstructured(module, name='weight', amount=ratio)
+                prune.remove(module, 'weight')
+                prune.l1_unstructured(module, name='bias', amount=ratio)
+                prune.remove(module, 'bias')
